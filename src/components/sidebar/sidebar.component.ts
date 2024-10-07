03c15375eb0fdd08001b6c76b6ad43832664da83
Rubens Borges <contact@rubensborges.dev>
2024-10-07 15:19:43 -0300

    feat: imported menu component on ts file

--- src/components/sidebar/sidebar.component.ts
+++ src/components/sidebar/sidebar.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import MenuItemsComponent from '../menu-items/menu-items.component';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [MenuItemsComponent],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  styleUrl: './sidebar.component.scss',
 })
-export class SidebarComponent {
-
-}
+export class SidebarComponent {}
