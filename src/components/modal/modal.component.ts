5631b50b270ec828eebd707999a802e41159dc29
Rubens Borges <contact@rubensborges.dev>
2024-10-11 14:22:31 -0300

    feat: added new methods to the modal class

--- src/components/modal/modal.component.ts
+++ src/components/modal/modal.component.ts
@@ -1,11 +1,32 @@
-import { Component, Input } from '@angular/core';
-import { HomeComponent } from '../home/home.component';
+// modal.component.ts
+import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-modal',
-  standalone: true,
-  imports: [],
   templateUrl: './modal.component.html',
-  styleUrl: './modal.component.scss',
+  imports: [CommonModule, FormsModule],
+  standalone: true,
+  styleUrls: ['./modal.component.scss'],
 })
-export class ModalComponent extends HomeComponent {}
+export class ModalComponent {
+  @Input() selectedDate: string = ''; // Receive selectedDate
+  @Input() patientName: string = ''; // Receive patientName
+  @Input() patientEmail: string = ''; // Receive patientEmail
+  @Output() closeModalEvent = new EventEmitter<void>(); // Emit event to close modal
+  @Output() addEventEvent = new EventEmitter<{ name: string; email: string }>(); // Emit new event
+
+  closeModal(): void {
+    this.closeModalEvent.emit(); // Emit close modal event
+  }
+
+  confirmEvent(): void {
+    // Emit new patient details
+    this.addEventEvent.emit({
+      name: this.patientName,
+      email: this.patientEmail,
+    });
+    this.closeModal(); // Close modal after adding event
+  }
+}
