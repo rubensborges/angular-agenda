2f6bc46fed5eef7ec4d5464e3e10d0f3117b3f2f
Rubens Borges <contact@rubensborges.dev>
2024-10-11 15:07:46 -0300

    fix: removed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from '../components/header/header.component';
-import { SidebarComponent } from '../components/sidebar/sidebar.component';
 import { HomeComponent } from '../components/home/home.component';
 import { FullCalendarModule } from '@fullcalendar/angular';
 
 @Component({
   selector: 'app-root',
-  standalone: true, // Standalone component flag
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    HomeComponent,
-    SidebarComponent,
-    FullCalendarModule, // Register FullCalendar here
-  ],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, FullCalendarModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'new-angular-project';
 
-  // Optional: Add calendar plugins and events for FullCalendar
   calendarPlugins = [];
   calendarEvents = [];
 }
