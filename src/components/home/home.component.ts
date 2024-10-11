3f9fcae1672f3393e4dcabbbffa55ffea013d2d2
Rubens Borges <contact@rubensborges.dev>
2024-10-11 15:05:12 -0300

    fix: class logic

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
@@ -6,72 +6,20 @@ import { CalendarOptions } from '@fullcalendar/core';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
 import timeGridPlugin from '@fullcalendar/timegrid';
-import { ChangeDetectorRef } from '@angular/core';
-import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FullCalendarModule, ModalComponent, CommonModule, FormsModule],
+  imports: [FullCalendarModule, CommonModule, FormsModule],
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent {
   isModalVisible: boolean = false;
   selectedDate: string = '';
   patientName: string = '';
   patientEmail: string = '';
   allEvents: any[] = [];
-
-  constructor(private cdr: ChangeDetectorRef) {}
-
-  ngOnInit(): void {
-    const storedPatients = localStorage.getItem('patients');
-    if (storedPatients) {
-      this.allEvents = JSON.parse(storedPatients);
-    }
-
-    this.initializeCalendarOptions();
-  }
-
-  initializeCalendarOptions(): void {
-    this.calendarOptions = {
-      initialView: 'timeGridWeek',
-      plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
-      headerToolbar: {
-        start: 'prev today next',
-        center: 'title',
-        right: 'dayGridMonth timeGridWeek timeGridDay',
-      },
-      height: '78vh',
-      nowIndicator: true,
-      eventColor: '#6666e2',
-      eventContent: (arg) => {
-        return {
-          html: `
-            <div>
-              <div>${arg.event.title}</div>
-              <div style="position: absolute; bottom: 0; font-size: 10px; color: #ccc; right: 1px;">
-                ${arg.timeText}
-              </div>
-            </div>
-          `,
-        };
-      },
-      locale: 'pt-BR',
-      slotLabelFormat: {
-        hour: '2-digit',
-        minute: '2-digit',
-      },
-      slotMinTime: '07:00:00',
-      slotMaxTime: '20:00:00',
-      slotDuration: '0:15:00',
-      dateClick: (arg) => this.handleDateClick(arg),
-      events: this.allEvents,
-      weekends: false,
-      eventDisplay: 'block',
-    };
-  }
 
   handleDateClick(arg: DateClickArg): void {
     this.selectedDate = arg.dateStr;
@@ -81,38 +29,70 @@ export class HomeComponent implements OnInit {
   closeModal(): void {
     this.isModalVisible = false;
   }
+
   formattedDate(): string {
     const date = new Date(this.selectedDate);
     date.setDate(date.getDate() + 1);
     return date.toLocaleDateString('pt-BR');
   }
 
-  addEvent(patient: { name: string; email: string }): void {
+  ngOnInit(): void {
+    localStorage.getItem('customers');
+  }
+
+  addEvent(): void {
     const newEvent = {
-      title: patient.name,
+      title: this.patientName,
       start: this.selectedDate,
-      email: patient.email,
+      description: this.patientEmail,
     };
 
-    this.allEvents = [...this.allEvents, newEvent];
-
-    this.updateLocalStorage();
+    this.allEvents.push(newEvent);
+    localStorage.setItem('customers', JSON.stringify(this.allEvents));
 
     this.calendarOptions = {
       ...this.calendarOptions,
-      events: this.allEvents,
+      events: [...this.allEvents],
     };
-
-    this.cdr.detectChanges();
 
     this.closeModal();
     this.patientName = '';
     this.patientEmail = '';
   }
 
-  updateLocalStorage(): void {
-    localStorage.setItem('patients', JSON.stringify(this.allEvents));
-  }
-
-  calendarOptions: CalendarOptions = {};
+  calendarOptions: CalendarOptions = {
+    initialView: 'timeGridWeek',
+    plugins: [dayGridPlugin, timeGridPlugin, interactionPlugin],
+    headerToolbar: {
+      start: 'prev today next',
+      center: 'title',
+      right: 'dayGridMonth timeGridWeek timeGridDay',
+    },
+    nowIndicator: true,
+    eventColor: '#6666e2',
+    eventContent: (arg) => {
+      return {
+        html: `
+          <div>
+            <div>${arg.event.title}</div>
+            <div style="position: absolute; bottom: 0; font-size: 10px; color: #ccc; right: 1px;">
+              ${arg.timeText}
+            </div>
+          </div>
+        `,
+      };
+    },
+    locale: 'pt-BR',
+    slotLabelFormat: {
+      hour: '2-digit',
+      minute: '2-digit',
+    },
+    slotMinTime: '07:00:00',
+    slotMaxTime: '20:00:00',
+    slotDuration: '0:15:00',
+    dateClick: (arg) => this.handleDateClick(arg),
+    events: this.allEvents,
+    weekends: false,
+    eventDisplay: 'block',
+  };
 }
